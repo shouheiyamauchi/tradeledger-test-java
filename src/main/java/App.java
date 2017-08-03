47b9918c3ae6203814c2b3b74af2894fe472649e
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-08-03 10:57:27 +1000

    update the sample app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,30 +2,12 @@ public class App {
     public static void main(String[] args) {
       FeeCalculator rule1 = new FeeCalculator(28, "0.04", "0.01", "invoiceAmount", "0.02");
 
-      System.out.println(rule1.getStandardRateDays());
-      System.out.println(rule1.getStandardRate());
-      System.out.println(rule1.getAdditionalRate());
-      System.out.println(rule1.getServiceFeeType());
-      System.out.println(rule1.getServiceFeeRate());
+      System.out.println(rule1.calcFee("80000", 26));
+      System.out.println(rule1.calcFee("80000", 45));
 
-      rule1.setStandardRateDays(29);
-      rule1.setStandardRate("0.05");
-      rule1.setAdditionalRate("0.02");
-      rule1.setServiceFeeType("invoiceAmountt");
-      rule1.setServiceFeeRate("0.03");
+      FeeCalculator rule2 = new FeeCalculator(20, "0.03", "0.01", "discountRate", "0.30");
 
-      System.out.println(rule1.getStandardRateDays());
-      System.out.println(rule1.getStandardRate());
-      System.out.println(rule1.getAdditionalRate());
-      System.out.println(rule1.getServiceFeeType());
-      System.out.println(rule1.getServiceFeeRate());
-
-      // System.out.println(rule1.calcFee(80000, 26));
-      // System.out.println(rule1.calcFee(80000, 45));
-      //
-      // FeeCalculator rule2 = new FeeCalculator(20, 0.03, 0.01, "discountRate", 0.30);
-      //
-      // System.out.println(rule2.calcFee(80000, 10));
-      // System.out.println(rule2.calcFee(80000, 35));
+      System.out.println(rule2.calcFee("80000", 10));
+      System.out.println(rule2.calcFee("80000", 35));
     }
 }
